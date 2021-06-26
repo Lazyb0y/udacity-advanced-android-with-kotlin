e0a30fa3b835a4491c90728fb1b131497a8a20f1
Foysal Ahamed <thisisfoysal@gmail.com>
2021-06-26 16:16:18 +0600

    Receive data messages

--- src/AndroidKotlinNotifications/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ src/AndroidKotlinNotifications/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -8,6 +8,20 @@ import com.google.firebase.messaging.RemoteMessage
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        remoteMessage?.data?.let {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        }
+    }
+
+    /**
      * Called if the FCM registration token is updated. This may occur if the security of
      * the previous token had been compromised. Note that this is called when the
      * FCM registration token is initially generated so this is where you would retrieve the token.
