179884c11cce314580ff7a055faeb4124d019d4f
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-02 19:39:45 +0600

    Add view interactivity

--- src/CustomFanController/app/src/main/java/com/example/android/customfancontrolle/DialView.kt
+++ src/CustomFanController/app/src/main/java/com/example/android/customfancontrolle/DialView.kt
@@ -13,6 +13,13 @@ private enum class FanSpeed(val label: Int) {
     LOW(R.string.fan_low),
     MEDIUM(R.string.fan_medium),
     HIGH(R.string.fan_high);
+
+    fun next() = when (this) {
+        OFF -> LOW
+        LOW -> MEDIUM
+        MEDIUM -> HIGH
+        HIGH -> OFF
+    }
 }
 
 private const val RADIUS_OFFSET_LABEL = 30
@@ -35,6 +42,10 @@ class DialView @JvmOverloads constructor(
         textAlign = Paint.Align.CENTER
         textSize = 55.0f
         typeface = Typeface.create("", Typeface.BOLD)
+    }
+
+    init {
+        isClickable = true
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -63,6 +74,16 @@ class DialView @JvmOverloads constructor(
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         radius = (min(width, height) / 2.0 * 0.8).toFloat()
+    }
+
+    override fun performClick(): Boolean {
+        if (super.performClick()) return true
+
+        fanSpeed = fanSpeed.next()
+        contentDescription = resources.getString(fanSpeed.label)
+
+        invalidate()
+        return true
     }
 
     private fun PointF.computeXYForSpeed(pos: FanSpeed, radius: Float) {
