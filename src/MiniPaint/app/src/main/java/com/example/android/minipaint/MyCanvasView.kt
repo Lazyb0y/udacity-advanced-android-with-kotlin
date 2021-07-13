59e9ea855600a35a7e51deae702ad3870cb2d8f2
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-13 21:11:49 +0600

    Implement touchStart()

--- src/MiniPaint/app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
+++ src/MiniPaint/app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -37,6 +37,9 @@ class MyCanvasView(context: Context) : View(context) {
     private var motionTouchEventX = 0f
     private var motionTouchEventY = 0f
 
+    private var currentX = 0f
+    private var currentY = 0f
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
@@ -64,7 +67,12 @@ class MyCanvasView(context: Context) : View(context) {
         extraCanvas.drawColor(backgroundColor)
     }
 
-    private fun touchStart() {}
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
 
     private fun touchMove() {}
 
