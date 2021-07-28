5c75b78c1272c99ccd866dfc6240a118e39eada2
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-28 12:32:15 +0600

    Adding markers on POI click

--- src/Wander/app/src/main/java/com/example/android/wander/MapsActivity.kt
+++ src/Wander/app/src/main/java/com/example/android/wander/MapsActivity.kt
@@ -52,6 +52,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map.addMarker(MarkerOptions().position(homeLatLng))
 
         setMapLongClick(map)
+        setPoiClick(map)
     }
 
     private fun setMapLongClick(map: GoogleMap) {
@@ -69,6 +70,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     .title(getString(R.string.dropped_pin))
                     .snippet(snippet)
             )
+        }
+    }
+
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            )
+            poiMarker.showInfoWindow()
         }
     }
 
