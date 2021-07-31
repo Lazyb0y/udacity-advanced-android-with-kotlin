5438b4009209884d4b842d34d3d598cf4aed92b3
Foysal Ahamed <thisisfoysal@gmail.com>
2021-07-31 21:52:26 +0600

    Added new test case for stats

--- src/AndroidTesting/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ src/AndroidTesting/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -21,4 +21,60 @@ class StatisticsUtilsTest {
         assertEquals(result.activeTasksPercent, 100f)
     }
 
+    @Test
+    fun getActiveAndCompletedStats_noActive_returnsZeroHundred() {
+
+        // Create an active tasks (the false makes this active)
+        val tasks = listOf<Task>(
+            Task("title", "desc", isCompleted = false)
+        )
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 100f)
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_both_returnsFortySixty() {
+
+        // Create an active tasks (the false makes this active)
+        val tasks = listOf<Task>(
+            Task("title", "desc", isCompleted = true),
+            Task("title", "desc", isCompleted = true),
+            Task("title", "desc", isCompleted = false),
+            Task("title", "desc", isCompleted = false),
+            Task("title", "desc", isCompleted = false)
+        )
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertEquals(result.completedTasksPercent, 40f)
+        assertEquals(result.activeTasksPercent, 60f)
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_error_returnsZeros() {
+
+        // Call our function
+        val result = getActiveAndCompletedStats(null)
+
+        // Check the result
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 0f)
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_empty_returnsZeros() {
+
+        // Call our function
+        val result = getActiveAndCompletedStats(emptyList())
+
+        // Check the result
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 0f)
+    }
+
 }
